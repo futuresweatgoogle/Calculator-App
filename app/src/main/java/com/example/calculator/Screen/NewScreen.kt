e7ccdcdc8505dedd0c69f154bc2aa0ba5be956ef
Ghulam Mustafa <mustafa.developer72@gmail.com>
2026-09-14 20:01:53 +0500

    few Changes

--- app/src/main/java/com/example/calculator/Screen/NewScreen.kt
+++ app/src/main/java/com/example/calculator/Screen/NewScreen.kt
@@ -1,11 +1,12 @@
 package com.example.calculator.Screen
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun CalculatorScreen(){
-
+    Text(text = "Mustafa")
 }
 
 @Preview(showSystemUi = true, showBackground = true, device = "id:pixel_6_pro")
