e5feca078f02562e9aa49b216c85560daa0979d0
Ghulam Mustafa <mustafa.developer72@gmail.com>
2026-09-14 19:40:04 +0500

    Calculator UI Screen

---
+++ app/src/main/java/com/example/calculator/Screen/NewScreen.kt
@@ -0,0 +1,15 @@
+package com.example.calculator.Screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CalculatorScreen(){
+
+}
+
+@Preview(showSystemUi = true, showBackground = true, device = "id:pixel_6_pro")
+@Composable
+private fun CalculatorScreenPreview(){
+    CalculatorScreen()
+}
