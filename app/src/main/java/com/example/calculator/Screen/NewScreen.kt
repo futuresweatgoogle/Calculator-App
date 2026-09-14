01eb963c4745fc413d8f29db3183c702ef9cc7f9
Ghulam Mustafa <mustafa.developer72@gmail.com>
2026-09-14 21:05:16 +0500

    UI Design

--- app/src/main/java/com/example/calculator/Screen/NewScreen.kt
+++ app/src/main/java/com/example/calculator/Screen/NewScreen.kt
@@ -1,12 +1,46 @@
 package com.example.calculator.Screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.intellij.lang.annotations.JdkConstants
 
 @Composable
 fun CalculatorScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(Color.Red)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(2.dp)
+                .height(200.dp)
+                .background(Color.Green),
 
+        ) {
+            Text(
+                text = "0",
+                fontSize = 46.sp
+            )
+        }
+    }
 }
 
 @Preview(showSystemUi = true, showBackground = true, device = "id:pixel_6_pro")
